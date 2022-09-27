514e7406ca8bed29f816f50fd67df32e444dfd0f
Isabella W <isabellawindhaber@gmail.com>
2022-09-27 15:47:58 +0200

    ....

--- CHALLENGE/index.js
+++ CHALLENGE/index.js
@@ -13,29 +13,93 @@ function displayPlayers () {
 displayPlayers();            
 
 
+let counter = 10;
+let player1 = 0;
+let player2 = 0;
 
 // onclick first button
 
 function buttonOne () {
 
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    player1 += Math.floor(Math.random() * 6) + 1;
 
     let dice = document.getElementById("diceHere1");          // .setAttribute("class", "pStyle")
-    dice.innerHTML = randomNumber;
+    dice.innerHTML = player1;
+
+    players();
 };
-
-buttonOne();
-
 
 
 // onclick second button
 
 function buttonTwo () {
 
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    player2 += Math.floor(Math.random() * 6) + 1;
 
     let dice = document.getElementById("diceHere2");
-    dice.innerHTML = randomNumber;
+    dice.innerHTML = player2;
+
+    players();
+
 };
 
-buttonTwo ();
+
+function players () {
+
+    if (counter%2 == 0) {
+    
+        console.log("player1"); 
+
+        counter--; 
+
+
+    document.getElementsByClassName("buttonPlayer1")[0].style.display = "block"; 
+    document.getElementsByClassName("buttonPlayer2")[0].style.display = "none";
+   
+    
+    }
+    
+
+    else {
+
+    console.log("player2");
+
+    counter--;
+
+    document.getElementsByClassName("buttonPlayer1")[0].style.display = "none";
+    document.getElementsByClassName("buttonPlayer2")[0].style.display = "block"; 
+
+
+    };
+
+    console.log(counter);
+
+
+    if (counter === 0) {
+
+        if (player1 > player2) {
+
+            alert("Player 1 WON! with " + player1 + " points");
+        }
+
+        else {
+            alert("Player 2 WON! with " + player2 + " points");
+
+        }
+
+    }
+    
+    /* if (counter === 9) {
+
+        document.getElementsByClassName("buttonPlayer1")[0].style.display = "none";
+        document.getElementsByClassName("buttonPlayer2")[0].style.display = "none";
+
+    } */
+    
+
+};
+
+players();
+
+
+
