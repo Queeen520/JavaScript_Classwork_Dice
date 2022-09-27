725eef28b5389a161b1aa5c8809ea95538f90f19
René Magnet <rene.m.magnet@gmail.com>
2022-09-27 13:13:19 +0200

    add

--- Basic-Exercise.js
+++ Basic-Exercise.js
@@ -15,3 +15,12 @@ let input1
     event.preventDefault();
   }*/
 
+
+  const input = document.querySelector('input');
+const log = document.getElementById('values');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
