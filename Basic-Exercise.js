ef6531eee988222b054f63e783ea22bea141735d
René Magnet <rene.m.magnet@gmail.com>
2022-09-27 11:39:06 +0200

    add

---
+++ Basic-Exercise.js
@@ -0,0 +1,17 @@
+document.getElementById("input1").addEventListener("click", displayInput1);
+
+document.getElementById("input2").addEventListener("click", displayInput1);
+
+document.getElementById("input3").addEventListener("click", displayInput1);
+
+
+function displayInput1() {
+let input1 
+}
+
+
+/*function  logSubmit() {
+    log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+    event.preventDefault();
+  }*/
+
